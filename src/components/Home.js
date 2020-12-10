1a195d69f6507565445246d7ae0f21e4a85a41bf
Ryan Paige <oshmagoshzz@gmail.com>
2020-12-10 05:39:58 -0500

    testdata flatlist added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,40 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, {useState} from 'react'
+import { StyleSheet, SafeAreaView, FlatList, Text, View, Image } from 'react-native';
+import testData from '../../testData';
+
+
+
 
 export default function Home() {
+    const [data, setData] = useState(testData);
+
+
+    const renderItem = ({ item }) => (
+      <>
+      
+      <Image source={{ uri: item.image }} style={{ width: 400, height: 400 }}/>
+      </>
+    );
+
     return (
         <View>
-            <Text>Initial</Text>
+            <SafeAreaView style={styles.container}>
+                <FlatList
+                style={styles.carousel}
+                horizontal
+                data={testData}
+                renderItem={renderItem}
+                keyExtractor={item => item.id}
+                />
+            </SafeAreaView>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
     },
 })
